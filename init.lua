5f4510b0e844645ae440cb703f0ebb198894ab3f
LamuneGit <lamune@naver.com>
2026-02-07 14:11:19 +0900

    줄복사

--- init.lua
+++ init.lua
@@ -6,7 +6,6 @@ require('lamune_common')        -- 공용기능 정의
 require('lamune_kor')           -- 한영 변환
 require('lamune_window')        -- 프로그램 창 제어 기능
 
---require('lamune_shortcut_move_key')     -- 단축키 지정 : 키보드에 없는 이동 키 구현
 require('lamune_shortcut_App')      -- 단축키 지정 : App 실행
 
 require('lamune_func_macro')       -- 실행하고자 하는 특수 기능들을 정의
@@ -15,7 +14,7 @@ require('lamune_shortcut_macro')    -- 단축키 지정 : 매크로 기능
 require('lamune_modifier')          -- 세부(왼/오 Modifier/ 다중클릭별) 기능 실행 구현
 
 require('lamune_vim_simple')           -- vim 키보드
-require('lamune_vim_number_keypad')           -- vim 키보드
+require('lamune_vim_number_keypad')           -- 숫자 키패드 키보드
 
 
 --require('lamune_vim')           -- vim 키보드
--- lamune_vim_simple.lua
+++ lamune_vim_simple.lua
@@ -6,108 +6,27 @@ local activeBindings = {}
 -- Key mappings for cursor mode
 local keyMappings = {
 
-    {t="key", from = {{},"["}, to = {{},"pageup"}},
-    {t="key", from = {{},"'"}, to = {{},"pagedown"}},
-    {t="key", from = {{},"u"}, to = {{},"home"}},
-    {t="key", from = {{},"o"}, to = {{},"end"}},
+    {t="key", from = {{},"u"}, to = {{},"pageup"}},
+    {t="key", from = {{},"o"}, to = {{},"pagedown"}},
+    {t="key", from = {{},"n"}, to = {{},"home"}},
+    {t="key", from = {{},"m"}, to = {{},"end"}},
     --{t="key", from = {{},"h"}, to = {{"cmd"}, "left"}}, -- beginning of line
     --{t="key", from = {{},";"}, to = {{"cmd"}, "right"}}, -- end of line
 
-    {t="key", from = {{"shift"},"["}, to = {{"shift"},"pageup"}},
-    {t="key", from = {{"shift"},"'"}, to = {{"shift"},"pagedown"}},
-    {t="key", from = {{"shift"},"u"}, to = {{"shift"},"home"}},
-    {t="key", from = {{"shift"},"o"}, to = {{"shift"},"end"}},
+    {t="key", from = {{"shift"},"u"}, to = {{"shift"},"pageup"}},
+    {t="key", from = {{"shift"},"o"}, to = {{"shift"},"pagedown"}},
+    {t="key", from = {{"shift"},"n"}, to = {{"shift"},"home"}},
+    {t="key", from = {{"shift"},"m"}, to = {{"shift"},"end"}},
     --{t="key", from = {{"shift"},"h"}, to = {{"shift","cmd"}, "left"}}, -- beginning of line
     --{t="key", from = {{"shift"},";"}, to = {{"shift","cmd"}, "right"}}, -- end of line
 
-    {t="key", from = {{"cmd"},"["}, to = {{"cmd"},"pageup"}},
-    {t="key", from = {{"cmd"},"'"}, to = {{"cmd"},"pagedown"}},
-    {t="key", from = {{"cmd"},"u"}, to = {{"cmd"},"home"}},
-    {t="key", from = {{"cmd"},"o"}, to = {{"cmd"},"end"}},
+    {t="key", from = {{"cmd"},"u"}, to = {{"cmd"},"pageup"}},
+    {t="key", from = {{"cmd"},"o"}, to = {{"cmd"},"pagedown"}},
+    {t="key", from = {{"cmd"},"n"}, to = {{"cmd"},"home"}},
+    {t="key", from = {{"cmd"},"m"}, to = {{"cmd"},"end"}},
+
 
     -------------------------------------------------------------------------
-
-
-    {t="fnc", from = {{},"p"}, to = function() -- ctrl + c
-        fastKeyStroke(mod.cmd, "c") 
-    end}, 
-    {t="fnc", from = {{"shift"},"p"}, to = function() -- ctrl + v 
-        fastKeyStroke(mod.cmd, "v") 
-    end}, 
-
-    {t="key", from = {{},"]"}, to = {{},"delete"}},         -- delete ⬅️
-    {t="key", from = {{},"\\"}, to = {{},"forwarddelete"}},  -- forward delete ➡️
-    
-    --{t="key", from = {{},"z"}, to = {{"cmd"},"z"}},         -- cmd + z --> 동작안함
-    --{t="key", from = {{},"x"}, to = {{"cmd"},"x"}},         -- cmd + z --> 동작안함
-    --{t="key", from = {{},"c"}, to = {{"cmd"},"c"}},         -- cmd + z --> 동작안함
-    --{t="key", from = {{},"v"}, to = {{"cmd"},"v"}},         -- cmd + z --> 동작안함
-
-    
-    ------------------------------------------------------------------------
-
-    
-    {t="fnc", from = {{},"."}, to = function() -- 한줄 복사
-        fastKeyStroke(mod.cmd, "right") 
-        fastKeyStroke(mod.cmd_and_shift, "left") 
-        fastKeyStroke(mod.cmd, "c") 
-    end}, 
-    {t="fnc", from = {{"shift"},"."}, to = insert_cur_line},  -- 현재 줄에 클립보드 삽입
-    -- cmd . => 숫자 3
-    {t="fnc", from = {{"cmd","shift"},"."}, to = function() -- 줄 복제
-        fastKeyStroke(mod.cmd_and_shift, "left") 
-        fastKeyStroke(mod.cmd_and_shift, "left") 
-        fastKeyStroke(mod.cmd_and_shift, "left") 
-        fastKeyStroke(mod.cmd_and_shift, "left") 
-        fastKeyStroke(mod.cmd, "right") 
-        fastKeyStroke(mod.cmd, "c") 
-        fastKeyStroke("cmd", "v")
-        fastKeyStroke({}, "home")
-    end}, 
-
-    {t="fnc", from = {{"cmd","alt"},"."}, to = function() -- 새로운 라인
-        fastKeyStroke({}, "end")
-        fastKeyStroke({}, "return")
-    end}, 
-
--------------------------------------------------------------------------
-    {t="fnc", from = {{},"/"}, to = function() -- 한줄 삭제
-        fastKeyStroke({}, "end")
-        fastKeyStroke({"shift"}, "home")
-        fastKeyStroke({"shift"}, "home")
-        fastKeyStroke({}, "forwarddelete")
-        fastKeyStroke({}, "forwarddelete")
-    end}, 
-    {t="fnc", from = {{"shift"},"/"}, to = function() -- 한줄 잘라내기
-        fastKeyStroke(mod.cmd, "right") 
-        fastKeyStroke(mod.cmd_and_shift, "left") 
-        fastKeyStroke(mod.cmd, "x")
-    end}, 
-
-    {t="fnc", from = {"cmd","/"}, to = function() -- 한줄 주석
-        fastKeyStroke({}, "home")
-        fastKeyStroke({}, "home")
-        
-        fastKeyStroke({}, "-")
-        fastKeyStroke({}, "-")
-        fastKeyStroke({}, "end")
-        fastKeyStroke({}, "home")
-        
-        fastKeyStroke({}, "down")
-    end}, 
-    {t="fnc", from = {{"cmd","alt"},"/"}, to = function() -- 한줄 주석 해제
-        fastKeyStroke({}, "home")
-        fastKeyStroke({}, "home")
-        
-        fastKeyStroke({}, "forwarddelete")
-        fastKeyStroke({}, "forwarddelete")
-        
-        fastKeyStroke({}, "down")
-    end}, 
-
-    
-    -------------------------------------------------------------------------
-
     {t="key", from = {{},"j"}, to = {{},"left"}},
     {t="key", from = {{},"l"}, to = {{},"right"}},
     {t="key", from = {{},"i"}, to = {{},"up"}},
@@ -122,18 +41,90 @@ local keyMappings = {
     {t="key", from = {{"shift"},"h"}, to = {{"shift","alt"}, "left"}}, -- back word
     {t="key", from = {{"shift"},";"}, to = {{"shift","alt"}, "right"}}, -- forward word  
 
+    
+    -------------------------------------------------------------------------
+    {t="fnc", from = {{},"p"}, to = function() -- ctrl + c
+        fastKeyStroke(mod.cmd, "c") 
+    end}, 
+    {t="fnc", from = {{"cmd"},"p"}, to = function() -- 뒤쪽 복사
+        fastKeyStroke({"cmd","shift"}, "right") 
+        fastKeyStroke(mod.cmd, "c") 
+    end}, 
+    {t="fnc", from = {{"alt"},"p"}, to = function() -- 앞쪽 복사 
+        fastKeyStroke({"cmd","shift"}, "left") 
+        fastKeyStroke(mod.cmd, "c") 
+    end},     
+    {t="fnc", from = {{"shift"},"p"}, to = function() -- 한줄 복사
+        fastKeyStroke(mod.cmd, "right")
+        fastKeyStroke(mod.cmd_and_shift, "left") 
+        fastKeyStroke(mod.cmd, "c") 
+    end}, 
+
+
+    ------------------------------------------------------------------------
+    {t="fnc", from = {{},"["}, to = function() -- ctrl + v 
+        fastKeyStroke(mod.cmd, "v") 
+    end}, 
+    {t="fnc", from = {{"cmd"},"["}, to = insert_cur_line},  -- 현재 줄에 클립보드 삽입
+
+
+    ------------------------------------------------------------------------
+    {t="key", from = {{},"'"}, to = {{},"delete"}},         -- delete ⬅️
+    {t="key", from = {{"cmd"},"'"}, to = {{},"forwarddelete"}},  -- forward delete ➡️
+
+
+    ------------------------------------------------------------------------
+    {t="fnc", from = {{},"."}, to = function() -- 새로운 라인
+        fastKeyStroke({}, "end")
+        fastKeyStroke({}, "return")
+    end}, 
+    {t="fnc", from = {{"cmd"},"."}, to = function() -- 새로운 라인 (위쪽)
+        fastKeyStroke({}, "up")
+        fastKeyStroke({}, "end")
+        fastKeyStroke({}, "return")
+    end}, 
+    {t="fnc", from = {{"shift"},"."}, to = copyLine }, -- 줄 복제
+
+
+    ------------------------------------------------------------------------
+    {t="fnc", from = {{},"/"}, to = deleteLine }, -- 한줄 삭제
+    {t="fnc", from = {{"shift"},"/"}, to = cutLine }, -- 한줄 잘라내기
+
+
+    -- {t="fnc", from = {"cmd","/"}, to = function() -- 한줄 주석
+    --     fastKeyStroke({}, "home")
+    --     fastKeyStroke({}, "home")
+        
+    --     fastKeyStroke({}, "-")
+    --     fastKeyStroke({}, "-")
+    --     fastKeyStroke({}, "end")
+    --     fastKeyStroke({}, "home")
+        
+    --     fastKeyStroke({}, "down")
+    -- end}, 
+    -- {t="fnc", from = {{"cmd","alt"},"/"}, to = function() -- 한줄 주석 해제
+    --     fastKeyStroke({}, "home")
+    --     fastKeyStroke({}, "home")
+        
+    --     fastKeyStroke({}, "forwarddelete")
+    --     fastKeyStroke({}, "forwarddelete")
+        
+    --     fastKeyStroke({}, "down")
+    -- end}, 
+
+
     -------------------------------------------------------------------------
 
-    {t="key", from = {{"cmd"},"n"}, to = {{},"0"}},
-    {t="key", from = {{"cmd"},"m"}, to = {{},"1"}},
-    {t="key", from = {{"cmd"},","}, to = {{},"2"}},
-    {t="key", from = {{"cmd"},"."}, to = {{},"3"}},
-    {t="key", from = {{"cmd"},"j"}, to = {{},"4"}},
-    {t="key", from = {{"cmd"},"k"}, to = {{},"5"}},
-    {t="key", from = {{"cmd"},"l"}, to = {{},"6"}},
-    {t="key", from = {{"cmd"},"u"}, to = {{},"7"}},
-    {t="key", from = {{"cmd"},"i"}, to = {{},"8"}},
-    {t="key", from = {{"cmd"},"o"}, to = {{},"9"}},
+    {t="key", from = {{"cmd","alt"},"b"}, to = {{},"0"}},
+    {t="key", from = {{"cmd","alt"},"n"}, to = {{},"1"}},
+    {t="key", from = {{"cmd","alt"},"m"}, to = {{},"2"}},
+    {t="key", from = {{"cmd","alt"},","}, to = {{},"3"}},
+    {t="key", from = {{"cmd","alt"},"j"}, to = {{},"4"}},
+    {t="key", from = {{"cmd","alt"},"k"}, to = {{},"5"}},
+    {t="key", from = {{"cmd","alt"},"l"}, to = {{},"6"}},
+    {t="key", from = {{"cmd","alt"},"u"}, to = {{},"7"}},
+    {t="key", from = {{"cmd","alt"},"i"}, to = {{},"8"}},
+    {t="key", from = {{"cmd","alt"},"o"}, to = {{},"9"}},
 
 
     
